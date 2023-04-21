c8c2b2c5b335aad74e11e22b14f996428d959854
Drustan <drustanyjt@gmail.com>
2023-04-21 21:24:22 +0800

    created a side bar

---
+++ src/SideBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const SideBar: React.FC = () => {
+
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
+    bg-blue-400 text-white shadow-sm">
+      <i>A</i>
+      <i>B</i>
+      <i>C</i>
+    </div>
+  )
+};
+
+export default SideBar;
