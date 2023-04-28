333660ccf5eee2666be6827a7bf75c40797f92eb
Drustan <drustanyjt@gmail.com>
2023-04-28 20:29:12 +0800

    Created some days, but no types yet

--- src/routes/TaiwanDay.tsx
+++ src/routes/TaiwanDay.tsx
@@ -1,17 +1,82 @@
 import { useParams } from "react-router-dom";
 
+function DayDetail(props : any) {
+  const details = [
+    {
+      dayId: 0,
+      toDo: [
+        {
+          event: "Arrive",
+          timeStart: "1500",
+          timeEnd: null,
+        },
+        {
+          event: "Reach Hotel",
+          timeStart: "1900",
+          timeEnd: null,
+        }
+      ],
+    },
+    {
+      dayId: 1,
+      toDo: [
+        {
+          event: "Breakfast",
+          timeStart: "0900",
+          timeEnd: "1000",
+        }
+      ],
+    }
+  ]
+
+  const dayId = props.dayId;
+  if (dayId < 0 || dayId >= details.length) {
+    return <p><b>Oops</b> no itinerary found</p>;
+  }
+
+  const detail = details[props.dayId];
+  return <Detail detail={detail} />;
+}
+
+function Detail(props : any) {
+    const detail = props.detail;
+
+    return (
+      <div className='flex flex-col gap-0'>
+        {detail.toDo.map((item: any, index: number) => {
+          return (
+            <div key={index} className="flex items-center gap-4">
+              <div className="font-bold">{item.timeStart +
+              (item.timeEnd ? `-${item.timeEnd}` : ``)}</div>
+              <div className="flex-1">{item.event}</div>
+            </div>);
+        })}
+      </div>
+    )
+}
+
 function TaiwanDay(props : any) {
 
   const { dayId } = useParams();
 
-
+  const testDetail = {
+    dayId: 1,
+    toDo: [
+      {
+        event: "Breakfast",
+        timeStart: "0900",
+        timeEnd: "1000",
+      }
+    ],
+  };
 
   return (
-    <div className='flex h-screen w-screen
-    justify-center text-center'>
-      <div>
+    <div className='flex flex-col w-screen
+    justify-center text-center items-center'>
+      <div className="flex">
         <p>Day {dayId}!</p>
       </div>
+      <DayDetail dayId={dayId}/>
     </div>
   )
 } 
